fa04aee8a696495fff55bd7f585be42f28016d27
ajfortuna97 <ana97@pitt.edu>
2018-08-31 16:19:22 -0400

    Debugged the random chance for cards, now works fully. Will need to add in functionality for all other card types later.

--- TouhouCardBotPublicRelease.py
+++ TouhouCardBotPublicRelease.py
@@ -9,6 +9,7 @@ import io
 from time import gmtime, strftime
 import time
 from datetime import datetime
+import sys
 
 import mysql
 import mysql.connector
@@ -20,13 +21,18 @@ db = mysql.connector.connect(user='[user]', password='[database password]', host
 @client.event
 @asyncio.coroutine
 def on_message(message):
+	print("Got message.")
+
+	if(message.content.startswith("quit")):
+		sys.exit()
+
 	author = message.author
 	messageServer = message.server
 	if messageServer.id == '[serverID]':  # this is to limit where the bot can go, you put your own server ID here
 		# 1/1000 chance to get a card
 		queryString = "SELECT * FROM userlist WHERE user = " + author.id
 		print(queryString + "\n")
-		cursor = db.cursor()
+		cursor = db.cursor(buffered=True)
 		cursor.execute(queryString)
 		curUser = cursor.fetchone()
 		#opLeaderName = str(topLeader[0])
@@ -38,7 +44,7 @@ def on_message(message):
 			#timeToList = datetime.now()
 			queryString = "INSERT INTO userlist VALUES(\'%s\', 500, \'%s\')" % (author.id, timeToList)
 			print(queryString + '\n')
-			cursor = db.cursor()
+			cursor = db.cursor(buffered=True)
 			cursor.execute(queryString)
 			#curUser = cursor.fetchone()
 			print("Added user.\n")
@@ -50,7 +56,7 @@ def on_message(message):
 		# nope no timing's yet
 		# because have to do research into converting between datetime
 		queryString = "SELECT lastrolled FROM userlist WHERE user = %s" % (author.id)
-		cursor = db.cursor() # yeah I can probably get away without doing this but copy paste
+		cursor = db.cursor(buffered=True) # yeah I can probably get away without doing this but copy paste
 		cursor.execute(queryString)
 		lastTimeRolled = cursor.fetchone() # so it will have been made by now
 		# we have the mython date time object now
@@ -63,12 +69,13 @@ def on_message(message):
 		difference = abs((rightNow - lastTimeRolledConverted))
 		print("Difference in Seconds: %s" % (difference.total_seconds()))
 
-		if(difference.total_seconds() >= 120):
+		#if(difference.total_seconds() >= 120):
+		if(difference.total_seconds() >= 0): # debugging line
 			print("I can roll.\n")
 			print("Updating time.\n")
 			timeToList = rightNow.strftime("%Y-%m-%d %H:%M:%S")
 			queryString = "UPDATE userlist SET lastrolled = \'%s\' WHERE user = %s" % (timeToList, author.id)
-			cursor = db.cursor()
+			cursor = db.cursor(buffered=True)
 			cursor.execute(queryString)
 			#lastTimeRolled = cursor.fetchone()
 			db.commit()
@@ -76,91 +83,124 @@ def on_message(message):
 
 			getCard = random.randint(1, 1000)
 
-			if(getCard==1000): # you get a random card
-				getCardNamesCount = "SELECT COUNT(*) FROM (SELECT name FROM cardcatalogue)"
-				cursor = db.cursor()
+			#if(getCard==1000): # you get a random card
+			if True is True: # this is for debugging
+				getCardNamesCount = "SELECT COUNT(*) FROM (SELECT name FROM cardcatalogue) AS getName"
+				cursor = db.cursor(buffered=True)
 				cursor.execute(getCardNamesCount)
 				cardTypesResult = cursor.fetchone()
 				totalCardTypes = cardTypesResult[0]
 
 				cardNameRolled = random.randint(1, totalCardTypes)
+				cardNameRolled = 1 # debugging line
+				print("You rolled card type %d" % cardNameRolled)
 
+
+				# BIG DECIDING PORTION ON WHO YOU GET HERE
+				touhouYouGet = ""
 				if(cardNameRolled==1): # Alice
+					touhouYouGet = "Alice"
 					# NOW YOU GACHA AGAIN FOR RARITY
-					rarityGet = random.randint(1,5)
-					getTotalOfNamed = "SELECT COUNT(*) FROM cardcatalogue WHERE rarity = %s AND name = 'Alice'" % (rarityGet)
-					cursor = db.cursor()
-					cursor.execute(getTotalOfNamed)
-					getTotalOfNamedResult = cursor.fetchone()
-					maxNumberOfType = getTotalOfNamedResult[0] # so if 1*s have 5 cards, this will give you five
-					numCardGet = random.randint(1,maxNumberOfType) # okay so now we have which card you get
-					# okay so now we have to assign it
-					# so first we have to get its ID and stuff
-					getReceivedCard = "SELECT * FROM cardcatalogue WHERE rarity = %s AND name = 'Alice' AND ROWNUM = %s" % (rarityGet, numCardGet)
-					cursor = db.cursor()
-					cursor.execute(getReceivedCard)
+
+
+
+				# handles rarity and the rest of the card business
+
+				rarityGet = random.randint(1,5)
+				print("You rolled rarity %d" % rarityGet)
+				getTotalOfNamed = "SELECT COUNT(*) FROM cardcatalogue WHERE rarity = %s AND name = \'%s\'" % (rarityGet, touhouYouGet)
+				cursor = db.cursor(buffered=True)
+				cursor.execute(getTotalOfNamed)
+				getTotalOfNamedResult = cursor.fetchone()
+				maxNumberOfType = getTotalOfNamedResult[0] # so if 1*s have 5 cards, this will give you five
+				numCardGet = random.randint(1,maxNumberOfType) # okay so now we have which card you get
+				#cursor.fetchall() # more dumb workarounds
+				# okay so now we have to assign it
+				# so first we have to get its ID and stuff
+				#getReceivedCard = "SELECT * FROM cardcatalogue WHERE rarity = %s AND name = \'%s\' AND ROWNUM = %s" % (rarityGet, numCardGet, touhouYouGet)
+
+				getReceivedCard = "SELECT * FROM cardcatalogue WHERE rarity = %s AND name = \'%s\'" % (rarityGet, touhouYouGet)
+				
+				numCardGetCounter = 0
+				cursor = db.cursor(buffered=True)
+				cursor.execute(getReceivedCard)
+				db.commit()
+
+				while(numCardGetCounter != numCardGet): # so if you had card 1, it loops once, 2, loops twice, etc.
 					cardYouJustGot = cursor.fetchone()
-					personCardNum = cardYouJustGot[3]
-					# okay so now we have whether it's Alice2, Alice3, etc.
-					# now we have to go to alice table
-					checkIfInPersonTable = "SELECT * FROM Alice WHERE user = \'%s\'" % (author.id)
-					cursor = db.cursor()
-					cursor.execute(checkIfInPersonTable)
-					inPersonTableResults = cursor.fetchone()
+					numCardGetCounter = numCardGetCounter + 1
 
-					if(inPersonTableResults == None):
-						# need to add them into the table
-						hasCardString = "hasCard%s" % (personCardNum) # so hasCard3
-						updaterString = "INSERT INTO Alice (user, %s) VALUES (%s, 1)" % (hasCardString, author.id)
-						cursor = db.cursor()
-						cursor.execute(updaterString)
+				# okay so this is a dumb workaround due to mySQL "unread results"
+				#cursor.fetchall() # done lol
+
+				personCardNum = cardYouJustGot[3]
+				# okay so now we have whether it's Alice2, Alice3, etc.
+				print("You got card %s%d" % (touhouYouGet, personCardNum)) 
+				# now we have to go to alice table
+				checkIfInPersonTable = "SELECT * FROM %s WHERE user = \'%s\'" % (touhouYouGet, author.id)
+				cursor = db.cursor(buffered=True)
+				cursor.execute(checkIfInPersonTable)
+				inPersonTableResults = cursor.fetchone()
+				db.commit()
+
+				if(inPersonTableResults == None):
+					# need to add them into the table
+					print("Adding you to the table to get the card\n")
+					hasCardString = "hasCard%s" % (personCardNum) # so hasCard3
+					updaterString = "INSERT INTO %s (user, %s) VALUES (%s, 1)" % (touhouYouGet, hasCardString, author.id)
+					cursor = db.cursor(buffered=True)
+					cursor.execute(updaterString)
+					db.commit()
+					# there we go, added them in
+				else:
+					# this means they are already in there
+					# so first let's check if they already have it
+					hasCardString = "hasCard%s" % (personCardNum) # so hasCard3
+					print("You already have a card of this type.\n")
+					checkerString = "SELECT user, %s FROM %s WHERE user=\'%s\'" % (hasCardString, touhouYouGet, author.id)
+					cursor = db.cursor(buffered=True)
+					cursor.execute(checkerString)
+					checkerResult = cursor.fetchone()
+					alreadyHas = checkerResult[1]
+					db.commit()
+
+					if(alreadyHas==1): # that is to say, already has the card, refund some points
+						print("User already has this card, refund points\n")
+						# NOW I GOTTA WRITE A QUERY TO GET YOUR FRIGGING POITNS
+						getPointsQuery = "SELECT points FROM userlist where user=\'%s\'" % (author.id)
+						cursor = db.cursor(buffered=True)
+						cursor.execute(getPointsQuery)
+						userPointsResult =  cursor.fetchone()
+						userPoints = userPointsResult[0]
+
+						# okay so now we need the rarity of the card
+						if rarityGet == 1:
+							userPoints = userPoints + 10
+						elif rarityGet == 2:
+							userPoints = userPoints + 20
+						elif rarityGet == 3:
+							userPoints = userPoints + 30
+						elif rarityGet == 4:
+							userPoints = userPoints + 40
+						elif rarityGet == 5:
+							userPoints = userPoints + 50
+
+						addPointsBackQuery = "UPDATE userlist SET points = %d WHERE user = \'%s\'" % (userPoints, author.id)
+						cursor = db.cursor(buffered=True)
+						cursor.execute(addPointsBackQuery)
+						# points added in
 						db.commit()
-						# there we go, added them in
+
 					else:
-						# this means they are already in there
-						# so first let's check if they already have it
-						checkerString = "SELECT user, %s FROM Alice WHERE user=\'%s\'" % (author.id)
-						cursor = db.cursor()
-						cursor.execute(checkerString)
-						checkerResult = cursor.fetchone()
-						alreadyHas = checkerResult[1]
-
-						if(alreadyHas==1): # that is to say, already has the card, refund some points
-							print("User already has this card, refund points")
-							# NOW I GOTTA WRITE A QUERY TO GET YOUR FRIGGING POITNS
-							getPointsQuery = "SELECT points FROM userlist where user=\'%s\'" % (author.id)
-							cursor = db.cursor()
-							cursor.execute(getPointsQuery)
-							userPointsResult =  cursor.fetchone()
-							userPoints = userPointsResult[0]
-
-							# okay so now we need the rarity of the card
-							if rarityGet == 1:
-								userPoints = userPoints + 10
-							elif rarityGet == 2:
-								userPoints = userPoints + 20
-							elif rarityGet == 3:
-								userPoints = userPoints + 30
-							elif rarityGet == 4:
-								userPoints = userPoints + 40
-							elif rarityGet == 5:
-								userPoints = userPoints + 50
-
-							addPointsBackQuery = "UPDATE points FROM userlist where user=\'%s\'" % (author.id)
-							cursor = db.cursor()
-							cursor.execute(addPointsBackQuery)
-							# points added in
-
-						else:
-							print("User does not have this card, let's add it to their list.")
-							# so we have to define hasCardString here
-							hasCardString = "hasCard%s" % (personCardNum)
-							# well actually looking back on it this code is redundant lmao
-							# will fix in the optimization patch
-							addNewCardString = "INSERT INTO Alice (user, %s) VALUES (%s, 1)" % (hasCardString, author.id)
-							cursor = db.cursor()
-							cursor.execute(addNewCardString)
-							db.commit()
+						print("User does not have this card, let's add it to their list.\n")
+						# so we have to define hasCardString here
+						hasCardString = "hasCard%s" % (personCardNum)
+						# well actually looking back on it this code is redundant lmao
+						# will fix in the optimization patch
+						addNewCardString = "UPDATE %s SET %s = 1 WHERE user = \'%s\'" % (touhouYouGet, hasCardString, author.id) #(user, %s) VALUES (%s, 1)" % (hasCardString, author.id)"
+						cursor = db.cursor(buffered=True)
+						cursor.execute(addNewCardString)
+						db.commit()
 
 
 
